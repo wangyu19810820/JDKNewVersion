77ea8e29a4c206cee7eb6e5120a23c995fcfe327
wangyu19810820 <gadfly0036_cn@sina.com>
2020-06-17 10:01:25 +0800

    jdbc

--- src/jdbc/BlobTest.java
+++ src/jdbc/BlobTest.java
@@ -2,12 +2,9 @@ package jdbc;
 
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.io.*;
+import java.sql.*;
+import java.util.Date;
 
 public class BlobTest {
 
@@ -16,11 +13,40 @@ public class BlobTest {
         String sql = "insert into customers(name, email, birth, photo) values (?, ?, ?, ?)";
         try (Connection connection = GetConnectionDemo.getConnection();
                 PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setObject(1, "wy");
-            statement.setObject(2, "wy@sina.com");
+            statement.setObject(1, "wy2");
+            statement.setObject(2, "wy2@sina.com");
             statement.setObject(3, "1981-08-20");
-            statement.setBlob(4, new FileInputStream(new File("src/jdbc/wy.jpg")));
+            statement.setBlob(4, new FileInputStream(new File("src/jdbc/wy2.jpg")));
             statement.execute();
+        }
+    }
+
+    @Test
+    public void testQuery() throws Exception {
+        String sql = "select id, name, email, birth, photo from customers where id = ?";
+        try(Connection connection = GetConnectionDemo.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, 3);
+            try(ResultSet rs = statement.executeQuery()) {
+                if (rs.next()) {
+                    int id = rs.getInt("id");
+                    String email = rs.getString("email");
+                    String name = rs.getString("name");
+                    Date date = new Date(rs.getDate("birth").getTime());
+                    Customer customer = new Customer(id, name, email, date);
+                    System.out.println(customer);
+                    Blob photo = rs.getBlob("photo");
+                    try (InputStream is = photo.getBinaryStream();
+                            OutputStream os = new FileOutputStream("src/jdbc/wy1.jpg")) {
+//                        is.transferTo(os);
+                        byte[] buffer = new byte[1024];
+                        int len;
+                        while ((len = is.read(buffer)) != -1) {
+                            os.write(buffer, 0, len);
+                        }
+                    }
+                }
+            }
         }
     }
 }
